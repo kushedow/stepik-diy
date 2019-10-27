5d179be044a984cbc6015e127f1b1823eb39bf39
kushedow <Polemika2020>
2019-10-27 06:56:24 +0300

    Fixing requirements

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-#  подключить фласк и инициализироать его
-
 from flask import *  # сперва подключим модуль
 
 app = Flask(__name__)  # объявим экземпляр фласка
-
-
 
 @app.route('/')
 def main():
 
     output = "Flask is up and running"
-
     return output
-
 
 app.run()  # запустим сервер на 8000 порту!
